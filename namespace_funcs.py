19bca7288070d5114b4d37ae4035648ed3df5224
SavelyLesnikov <166781803+SavelyLesnikov@users.noreply.github.com>
2024-04-28 12:30:39 +0700

    Add files via upload

---
+++ namespace_funcs.py
@@ -0,0 +1,13 @@
+def test():
+    a, b = 28, 65
+    print(a, b)
+
+
+def test2(p1='How', p2='you', p3='doing?'):
+    print(p1)
+    print(p2)
+    print(p3)
+
+
+test()
+test2("I'm fine,", 'thank', 'you.')
